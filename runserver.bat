1ffe731e0c44c74860d1f91f38258f9e83e294ba
Kevin <kevin@eathena.ws>
2005-05-23 04:03:20 +0000

    fixed the previous fix >.<
    git-svn-id: http://eathena.googlecode.com/svn/branches/stable@1819 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- runserver.bat
+++ runserver.bat
@@ -4,6 +4,6 @@ rem It will also keep the map server OPEN after it crashes to that errors may be
 rem more easily identified
 rem Writen by Jbain
 echo Jbain's eAthena Start script for Windoze
-start cmd /k login-start.bat
-start cmd /k char-start.bat
-start cmd /k map-start.bat
+start cmd /k logserv.bat
+start cmd /k charserv.bat
+start cmd /k mapserv.bat
