65cd0a1c4f3f706357cd10a242004560e396970c
DracoRPG <dracorpg@eathena.ws>
2005-06-05 13:44:22 +0000

    Added a Lua script file
    git-svn-id: http://eathena.googlecode.com/svn/branches/lua@2060 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

---
+++ script/event_example.lua
@@ -0,0 +1,17 @@
+function PCLoginEvent(id)
+	print("Player with ID "..id.." logged in !")
+end
+
+function PCLogoutEvent(id)
+	print("Player with ID "..id.." logged out !")
+end
+
+function PCKillEvent(id)
+	print("Player with ID "..id.." killed someone !")
+end
+
+function PCDieEvent(id)
+	print("Player with ID "..id.." died !")
+end
+
+print "Event example successfully loaded !"
