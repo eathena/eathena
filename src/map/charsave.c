afbf5fd8e4bf37f0be35e2e903f17100d339bb1e
sirius <sirius@eathena.ws>
2005-06-17 18:47:32 +0000

    temp commit #3 of 4(sorry) sql new charsave method
    git-svn-id: http://eathena.googlecode.com/svn/branches/stable@2242 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

---
+++ src/map/charsave.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../common/core.h"
+#include "../common/socket.h"
+#include "../common/timer.h"
+#include "../common/nullpo.h"
+
+#include "charsave.h"
+#include "map.h"
+
+
+
+
+
---
+++ src/map/charsave.h
@@ -0,0 +1,7 @@
+#ifndef _CHARSAVE_H_
+#define _CHARSAVE_H_
+
+
+
+
+#endif
