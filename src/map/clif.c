524a7d25bed7f80f68afff754eab7e43303b89cd
Skotlex <skotlex@mailforce.net>
2005-07-18 00:14:11 +0000

    - GAH, more fixes on the previous debug messages...
    git-svn-id: http://eathena.googlecode.com/svn/branches/stable@2542 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- src/map/clif.c
+++ src/map/clif.c
@@ -11643,8 +11643,9 @@ int clif_parse(int fd) {
 		if (packet_ver < 5)
 		{
 			int i;
+			packet_len = RFIFOREST(fd);
 			if (fd)
-				ShowDebug("\nclif_parse: session #%d, packet 0x%x, length %d, version %d\n", fd, cmd, RFIFOREST(fd), packet_ver);
+				ShowDebug("\nclif_parse: session #%d, packet 0x%x, length %d, version %d\n", fd, cmd, packet_len, packet_ver);
 			printf("---- 00-01-02-03-04-05-06-07-08-09-0A-0B-0C-0D-0E-0F");
 			for(i = 0; i < packet_len; i++) {
 				if ((i & 15) == 0)
