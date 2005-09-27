1f933fda987dcd60def49149388ba81c00d79b84
Skotlex <skotlex@mailforce.net>
2005-09-27 00:57:58 +0000

    - Re-changed the timer minimal interval back to 50, who changed it to 25? o.O
    git-svn-id: http://eathena.googlecode.com/svn/branches/stable@3296 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- src/common/timer.c
+++ src/common/timer.c
@@ -29,7 +29,7 @@
 // If the server shows no reaction when processing thousands of monsters
 // or connected by many clients, please increase TIMER_MIN_INTERVAL.
 
-#define TIMER_MIN_INTERVAL 25
+#define TIMER_MIN_INTERVAL 50
 
 static struct TimerData* timer_data	= NULL;
 static int timer_data_max	= 0;
