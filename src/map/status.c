a1980cc9fce3c7b5032fe706e543cf191dbfe86d
Skotlex <skotlex@mailforce.net>
2005-10-22 01:59:44 +0000

    - Probably fixed reverse orcish not working.
    git-svn-id: http://eathena.googlecode.com/svn/branches/stable@3656 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- src/map/status.c
+++ src/map/status.c
@@ -4177,6 +4177,7 @@ int status_change_start(struct block_list *bl,int type,int val1,int val2,int val
 		case SC_HIGHJUMP:
 		case SC_MODE:
 		case SC_LONGING:
+		case SC_ORCISH:
 			break;
 
 		default:
