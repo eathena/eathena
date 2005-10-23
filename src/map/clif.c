cc794a3d02deeccd2e5951e6228e56c7ed5c7082
Skotlex <skotlex@mailforce.net>
2005-10-23 19:03:07 +0000

    - Fixed the error message when players request the name of a skill
    git-svn-id: http://eathena.googlecode.com/svn/branches/stable@3693 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- src/map/clif.c
+++ src/map/clif.c
@@ -7834,6 +7834,8 @@ int clif_charnameack (int fd, struct block_list *bl)
 			}
 		}
 		break;
+	case BL_SKILL: //Players may be requesting the name of a trap/icewall [Skotlex]
+		return 0;
 	default:
 		if (battle_config.error_log)
 			ShowError("clif_parse_GetCharNameRequest : bad type %d(%d)\n", bl->type, bl->id);
