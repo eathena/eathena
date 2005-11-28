b05b7dd3f58470bf4225e7876f45e62db9e41b77
Lance <lance@eathena.ws>
2005-11-28 09:00:35 +0000

    - Typo.. heheh..
    git-svn-id: http://eathena.googlecode.com/svn/branches/stable@4181 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- src/map/mob.c
+++ src/map/mob.c
@@ -2155,7 +2155,7 @@ int mob_damage(struct block_list *src,struct mob_data *md,int damage,int type)
 		return 0;
 	}
 
-	if(md->sc_date[SC_ENDURE].timer == -1) // Stop the walking [Lance]
+	if(md->sc_data[SC_ENDURE].timer == -1) // Stop the walking [Lance]
 		mob_stop_walking(md,1);
 
 	if(damage > max_hp>>2)
