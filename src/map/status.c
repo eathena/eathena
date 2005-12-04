c3fd92ec85c9e171b7692292dc56476ad485b64a
Lupus <Lupus@eathena.ws>
2005-12-04 14:53:01 +0000

    fixed VVS too big damage from TOP10 BS
    git-svn-id: http://eathena.googlecode.com/svn/branches/stable@4319 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- src/map/status.c
+++ src/map/status.c
@@ -1104,7 +1104,7 @@ int status_calc_pc(struct map_session_data* sd,int first)
 					if(sd->status.inventory[index].card[0]==0x00ff){	// Forged weapon
 						sd->left_weapon.star = (sd->status.inventory[index].card[1]>>8);	// ¯‚Ì‚©‚¯‚ç
 						if(sd->left_weapon.star >= 15) sd->left_weapon.star = 40; // 3 Star Crumbs now give +40 dmg
-						sd->left_weapon.star += 10 * pc_istop10fame( MakeDWord(sd->status.inventory[index].card[2],sd->status.inventory[index].card[3]) ,MAPID_BLACKSMITH);
+						sd->left_weapon.star += 10 * (0!=pc_istop10fame( MakeDWord(sd->status.inventory[index].card[2],sd->status.inventory[index].card[3]) ,MAPID_BLACKSMITH));
 						wele_= (sd->status.inventory[index].card[1]&0x0f);	// ? «
 					}
 					sd->attackrange_ += sd->inventory_data[index]->range;
@@ -1124,7 +1124,7 @@ int status_calc_pc(struct map_session_data* sd,int first)
 					if(sd->status.inventory[index].card[0]==0x00ff){	// Forged weapon
 						sd->right_weapon.star += (sd->status.inventory[index].card[1]>>8);	// ¯‚Ì‚©‚¯‚ç
 						if(sd->right_weapon.star >= 15) sd->right_weapon.star = 40; // 3 Star Crumbs now give +40 dmg
-						sd->right_weapon.star += 10 * pc_istop10fame( MakeDWord(sd->status.inventory[index].card[2],sd->status.inventory[index].card[3]) ,MAPID_BLACKSMITH);
+						sd->right_weapon.star += 10 * (0!=pc_istop10fame( MakeDWord(sd->status.inventory[index].card[2],sd->status.inventory[index].card[3]) ,MAPID_BLACKSMITH));
 						wele = (sd->status.inventory[index].card[1]&0x0f);	// ? «
 					}
 					sd->attackrange += sd->inventory_data[index]->range;
