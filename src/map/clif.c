ba7a2faa6c65062857ad8e30f67fa652b0cdf4c3
Skotlex <skotlex@mailforce.net>
2005-12-05 21:00:22 +0000

    - Added a bunch of missing NPC skills to the skill name listing in skill.c - Updated skill_get_name to return UNKNOWN_SKILL for all skills with no name. - Uncommented the use of skill_get_name in clif.c as the crash should be gone with the previous fix.
    git-svn-id: http://eathena.googlecode.com/svn/branches/stable@4336 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- src/map/clif.c
+++ src/map/clif.c
@@ -4527,8 +4527,7 @@ int clif_skillinfo(struct map_session_data *sd,int skillid,int type,int range)
 		range = skill_get_range2(&sd->bl, id,sd->status.skill[skillid].lv);
 
 	WFIFOW(fd,12)= range;
-	memset(WFIFOP(fd,14),0,NAME_LENGTH);
-//	strncpy(WFIFOP(fd,14), skill_get_name(id), NAME_LENGTH); //TODO: Correct a crash here when GM all SKILL is set. [Skotlex]
+	strncpy(WFIFOP(fd,14), skill_get_name(id), NAME_LENGTH);
 	inf2 = skill_get_inf2(id);
 	if(((!(inf2&INF2_QUEST_SKILL) || battle_config.quest_skill_learn) &&
 		!(inf2&(INF2_WEDDING_SKILL|INF2_SPIRIT_SKILL))) ||
@@ -4563,8 +4562,7 @@ int clif_skillinfoblock(struct map_session_data *sd)
 			WFIFOW(fd,len+6) = sd->status.skill[i].lv;
 			WFIFOW(fd,len+8) = skill_get_sp(id,sd->status.skill[i].lv);
 			WFIFOW(fd,len+10)= skill_get_range2(&sd->bl, id,sd->status.skill[i].lv);
-			memset(WFIFOP(fd,len+12),0,NAME_LENGTH);
-//			strncpy(WFIFOP(fd,len+12), skill_get_name(id), NAME_LENGTH); //TODO: Correct a crash here when GM all SKILL is set. [Skotlex]
+			strncpy(WFIFOP(fd,len+12), skill_get_name(id), NAME_LENGTH);
 			inf2 = skill_get_inf2(id);
 			if(((!(inf2&INF2_QUEST_SKILL) || battle_config.quest_skill_learn) &&
 				!(inf2&(INF2_WEDDING_SKILL|INF2_SPIRIT_SKILL))) ||
