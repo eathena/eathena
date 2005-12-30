59e72bf21df7a2f89747e924b4d4cdf55e1adb74
Skotlex <skotlex@mailforce.net>
2005-12-30 19:16:14 +0000

    - Updated mapindex so that name lookups ignore extensions, and the loaded maps are always terminated in .gat. Id lookups will always return a string terminated in gat. Purpose? It enables specifying mapnames without the .gat extension, so you can have npcs in prontera, no need for the .gat. And the returned string always has a .gat for needed when it is passed to the client.
    git-svn-id: http://eathena.googlecode.com/svn/branches/stable@4734 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- src/common/mapindex.c
+++ src/common/mapindex.c
@@ -6,7 +6,11 @@
 #define MAX_MAPINDEX 2000
 
 //Leave an extra char of space to hold the terminator, in case for the strncpy(mapindex_id2name()) calls.
-char indexes[MAX_MAPINDEX][MAP_NAME_LENGTH+1];
+struct {
+	char name[MAP_NAME_LENGTH+1]; //Stores map name
+	int length; //Stores string length WITHOUT the extension for quick lookup.
+} indexes[MAX_MAPINDEX];
+
 static unsigned short max_index = 0;
 
 char mapindex_cfgfile[80] = "db/map_index.txt";
@@ -14,14 +18,33 @@ char mapindex_cfgfile[80] = "db/map_index.txt";
 unsigned short mapindex_name2id(char* name) {
 	//TODO: Perhaps use a db to speed this up? [Skotlex]
 	int i;
+	int length = strlen(name);
+	char *ext = strstr(name, ".");
+	if (ext)
+		length = ext-name; //Base map-name length without the extension.
 	for (i = 1; i < max_index; i++)
 	{
-		if (indexes[i][0] && strncmp(indexes[i],name,MAP_NAME_LENGTH)==0)
+		if (indexes[i].length == length && strncmp(indexes[i].name,name,length)==0)
 			return i;
 	}
 #ifdef MAPINDEX_AUTOADD
 	if (i < MAX_MAPINDEX) {
-		strncpy(indexes[i], name, MAP_NAME_LENGTH);
+		char map_name[MAP_NAME_LENGTH+5];
+		length = strlen(name);
+		if (length > MAP_NAME_LENGTH)
+			return;
+		memcpy(map_name, name, length+1);
+		if ((ext = strstr(map_name, ".")) != NULL) {
+			length = ext-map_name;
+			sprintf(ext, ".gat");
+		} else { //No extension?
+			length = strlen(map_name);
+			strcat(map_name, ".gat");
+		}
+		if (length > MAP_NAME_LENGTH - 4)
+			return 0; //Can't be added.
+		strncpy(indexes[i].name, map_name, MAP_NAME_LENGTH);
+		indexes[i].length = strlen(map_name);
 		ShowDebug("mapindex_name2id: Added map \"%s\" to position %d\n", indexes[i], i);
 		return i;
 	}
@@ -31,18 +54,19 @@ unsigned short mapindex_name2id(char* name) {
 }
 
 char* mapindex_id2name(unsigned short id) {
-	if (id > MAX_MAPINDEX || !indexes[id][0]) {
+	if (id > MAX_MAPINDEX || !indexes[id].length) {
 		ShowDebug("mapindex_id2name: Requested name for non-existant map index [%d] in cache.\n", id);
-		return indexes[0]; //Theorically this should never happen, hence we return this string to prevent null pointer crashes.
+		return indexes[0].name; //Theorically this should never happen, hence we return this string to prevent null pointer crashes.
 	}
-	return indexes[id];
+	return indexes[id].name;
 }
 
 void mapindex_init() {
 	FILE *fp;
 	char line[1024];
+	char *ext;
 	int last_index = -1;
-	int index;
+	int index, length;
 	char map_name[1024];
 	
 	memset (&indexes, 0, sizeof (indexes));
@@ -55,7 +79,7 @@ void mapindex_init() {
 		if(line[0] == '/' && line[1] == '/')
 			continue;
 
-		switch (sscanf(line,"%s\t%d",map_name,&index)) {
+		switch (sscanf(line,"%1000s\t%d",map_name,&index)) {
 			case 1: //Map with no ID given, auto-assign
 				index = last_index+1;
 			case 2: //Map with ID given
@@ -63,9 +87,33 @@ void mapindex_init() {
 					ShowError("(mapindex_init) Map index (%d) for \"%s\" out of range (max is %d)\n", index, map_name, MAX_MAPINDEX);
 					continue;
 				}
-				if (indexes[index][0])
-					ShowWarning("(mapindex_init) Overriding index %d: map \"%s\" -> \"%s\"\n", indexes[index], map_name);
-				strncpy(indexes[index], map_name, MAP_NAME_LENGTH);
+				length = strlen(map_name);
+				if (length > MAP_NAME_LENGTH) {
+					ShowError("(mapindex_init) Map name %s is too long. Maps are limited to %d characters.\n", map_name, MAP_NAME_LENGTH);
+					continue;
+				}
+				if ((ext = strstr(map_name, ".gat")) != NULL) { //Gat map
+					length = ext-map_name;
+				} else if ((ext = strstr(map_name, ".afm")) != NULL || (ext = strstr(map_name, ".af2")) != NULL) { //afm map
+					length = ext-map_name;
+					sprintf(ext, ".gat"); //Change the extension to gat
+				} else if ((ext = strstr(map_name, ".")) != NULL) { //Generic extension?
+					length = ext-map_name;
+					sprintf(ext, ".gat");
+				} else { //No extension?
+					length = strlen(map_name);
+					strcat(map_name, ".gat");
+				}
+				if (length > MAP_NAME_LENGTH - 4) {
+					ShowError("(mapindex_init) Adjusted Map name %s is too long. Maps are limited to %d characters.\n", map_name, MAP_NAME_LENGTH);
+					continue;
+				}
+						
+				if (indexes[index].length)
+					ShowWarning("(mapindex_init) Overriding index %d: map \"%s\" -> \"%s\"\n", indexes[index].name, map_name);
+				
+				strncpy(indexes[index].name, map_name, MAP_NAME_LENGTH);
+				indexes[index].length = length;
 				if (max_index < index)
 					max_index = index;
 				break;
