41b99c7400bac783f7362af80a020a093985d3d1
Skotlex <skotlex@mailforce.net>
2006-02-17 03:39:47 +0000

    - Removed the check that prevented mobs from walking into a basilica.
    git-svn-id: http://eathena.googlecode.com/svn/branches/stable@5304 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- src/map/mob.c
+++ src/map/mob.c
@@ -541,12 +541,12 @@ static int mob_walk(struct mob_data *md,unsigned int tick,int data)
 		md->dir=md->walkpath.path[md->walkpath.path_pos];
 		dx = dirx[md->dir];
 		dy = diry[md->dir];
-
+/*
 		if (map_getcell(md->bl.m,x+dx,y+dy,CELL_CHKBASILICA) && !(status_get_mode(&md->bl)&MD_BOSS)) {
 			mob_stop_walking(md,1);
 			return 0;
 		}
-
+*/
 		if (map_getcell(md->bl.m,x+dx,y+dy,CELL_CHKNOPASS)) {
 			mob_walktoxy_sub(md);
 			return 0;
@@ -906,11 +906,12 @@ static int mob_walktoxy_sub(struct mob_data *md)
 		return 1;	
 	x = md->bl.x+dirx[wpd.path[0]];
 	y = md->bl.y+diry[wpd.path[0]];
+/*
 	if (map_getcell(md->bl.m,x,y,CELL_CHKBASILICA) && !(status_get_mode(&md->bl)&MD_BOSS)) {
 		md->state.change_walk_target=0;
 		return 1;
 	}
-
+*/
 	memcpy(&md->walkpath,&wpd,sizeof(wpd));
 
 	md->state.change_walk_target=0;
