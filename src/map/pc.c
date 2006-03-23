f3d1e8384a1154799c9ec46731a7767f6631d8cd
Skotlex <skotlex@mailforce.net>
2006-03-23 20:01:48 +0000

    - Fixed Devotion not properly being cleared on death in some cases. - Fixed Shield-Reflect not being correctly ended when devotion does. - Updated const.txt from trunk.
    git-svn-id: http://eathena.googlecode.com/svn/branches/stable@5721 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- src/map/pc.c
+++ src/map/pc.c
@@ -5355,6 +5355,7 @@ int pc_damage(struct block_list *src,struct map_session_data *sd,int damage)
 		if (sd->devotion[i]){
 			struct map_session_data *devsd = map_id2sd(sd->devotion[i]);
 			if (devsd) status_change_end(&devsd->bl,SC_DEVOTION,-1);
+			sd->devotion[i] = 0;
 		}
 
 	pc_setdead(sd);
