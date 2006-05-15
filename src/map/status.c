f7ec9f77e27e3945863150067f37f8c36a5c0bd7
Skotlex <skotlex@mailforce.net>
2006-05-15 18:26:24 +0000

    - Added the missing case SC_BLADESTOP on the main switch of status_change_start. May fix it.
    git-svn-id: http://eathena.googlecode.com/svn/branches/stable@6601 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- src/map/status.c
+++ src/map/status.c
@@ -4564,6 +4564,7 @@ int status_change_start(struct block_list *bl,int type,int rate,int val1,int val
 		case SC_SLEEP:
 		case SC_SMA:
 		case SC_WARM: 
+		case SC_BLADESTOP:
 			break;
 
 		default:
