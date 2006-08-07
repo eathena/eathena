a1bbf4e76e1b24a2758dd692f1956d64d11d94d2
Skotlex <skotlex@mailforce.net>
2006-08-07 20:50:06 +0000

    - SC_GUILDAURA is now removed on logout. - Merged updated debuff_on_logout setting which enables you to determine whether negative/positive/both status changes get cleared on logout. - Merged ending the Cooking status changes on respawn.
    git-svn-id: http://eathena.googlecode.com/svn/branches/stable@8176 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- src/map/unit.c
+++ src/map/unit.c
@@ -1598,7 +1598,9 @@ int unit_free(struct block_list *bl) {
 				status_change_end(bl,SC_BERSERK,-1);
 			if(sd->sc.data[SC_TRICKDEAD].timer!=-1)
 				status_change_end(bl,SC_TRICKDEAD,-1);
-			if (battle_config.debuff_on_logout) {
+			if(sd->sc.data[SC_GUILDAURA].timer!=-1)
+				status_change_end(bl,SC_GUILDAURA,-1);
+			if (battle_config.debuff_on_logout&1) {
 				if(sd->sc.data[SC_ORCISH].timer!=-1)
 					status_change_end(bl,SC_ORCISH,-1);
 				if(sd->sc.data[SC_STRIPWEAPON].timer!=-1)
@@ -1613,6 +1615,31 @@ int unit_free(struct block_list *bl) {
 					status_change_end(bl,SC_EXTREMITYFIST,-1);
 				if(sd->sc.data[SC_EXPLOSIONSPIRITS].timer!=-1)
 					status_change_end(bl,SC_EXPLOSIONSPIRITS,-1);
+			}
+			if (battle_config.debuff_on_logout&2)
+			{	//Food items are removed on logout.
+				if(sd->sc.data[SC_STRFOOD].timer!=-1)
+					status_change_end(bl,SC_STRFOOD,-1);
+				if(sd->sc.data[SC_AGIFOOD].timer!=-1)
+					status_change_end(bl,SC_AGIFOOD,-1);
+				if(sd->sc.data[SC_VITFOOD].timer!=-1)
+					status_change_end(bl,SC_VITFOOD,-1);
+				if(sd->sc.data[SC_INTFOOD].timer!=-1)
+					status_change_end(bl,SC_INTFOOD,-1);
+				if(sd->sc.data[SC_DEXFOOD].timer!=-1)
+					status_change_end(bl,SC_DEXFOOD,-1);
+				if(sd->sc.data[SC_LUKFOOD].timer!=-1)
+					status_change_end(bl,SC_LUKFOOD,-1);
+				if(sd->sc.data[SC_HITFOOD].timer!=-1)
+					status_change_end(bl,SC_HITFOOD,-1);
+				if(sd->sc.data[SC_FLEEFOOD].timer!=-1)
+					status_change_end(bl,SC_FLEEFOOD,-1);
+				if(sd->sc.data[SC_BATKFOOD].timer!=-1)
+					status_change_end(bl,SC_BATKFOOD,-1);
+				if(sd->sc.data[SC_WATKFOOD].timer!=-1)
+					status_change_end(bl,SC_WATKFOOD,-1);
+				if(sd->sc.data[SC_MATKFOOD].timer!=-1)
+					status_change_end(bl,SC_MATKFOOD,-1);
 			}
 		}
 		if (sd->followtimer != -1)
