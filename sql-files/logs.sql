c7ce9a795f091c5839fa4cafb2beff97bb9ce77e
Skotlex <skotlex@mailforce.net>
2006-10-31 14:37:56 +0000

    - Updated logs.sql - Added upgrade_svn9106.sql which adds some important indexes to the sql logs, should speed up log queries.
    git-svn-id: http://eathena.googlecode.com/svn/branches/stable@9106 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- sql-files/logs.sql
+++ sql-files/logs.sql
@@ -17,7 +17,8 @@ CREATE TABLE `picklog` (
   `card2` int(11) NOT NULL default '0',
   `card3` int(11) NOT NULL default '0',
   `map` varchar(20) NOT NULL default 'prontera.gat',
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY  (`id`),
+  INDEX (`type`)
 ) TYPE=MyISAM AUTO_INCREMENT=1 ;
 
 #ZenyLog types (M)onsters,(T)rade,(V)ending Sell/Buy,(S)hop Sell/Buy,(N)PC Change amount,(A)dministrators
@@ -31,7 +32,8 @@ CREATE TABLE `zenylog` (
   `type` set('M','T','V','S','N','A') NOT NULL default 'S',
   `amount` int(11) NOT NULL default '0',
   `map` varchar(20) NOT NULL default 'prontera.gat',
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY  (`id`),
+  INDEX (`type`)
 ) TYPE=MyISAM AUTO_INCREMENT=1 ;
 
 #Database: log
@@ -43,28 +45,9 @@ CREATE TABLE `branchlog` (
   `char_id` int(11) NOT NULL default '0',
   `char_name` varchar(30) NOT NULL default '',
   `map` varchar(20) NOT NULL default 'prontera.gat',
-  PRIMARY KEY  (`branch_id`)
-) TYPE=MyISAM AUTO_INCREMENT=1 ;
-
-#Database: log
-#Table: droplog
-CREATE TABLE `droplog` (
-  `drop_id` mediumint(9) unsigned NOT NULL auto_increment,
-  `drop_date` datetime NOT NULL default '0000-00-00 00:00:00',
-  `kill_char_id` int(11) NOT NULL default '0',
-  `monster_id` smallint(6) NOT NULL default '0',
-  `item1` int(11) NOT NULL default '0',
-  `item2` int(11) NOT NULL default '0',
-  `item3` int(11) NOT NULL default '0',
-  `item4` int(11) NOT NULL default '0',
-  `item5` int(11) NOT NULL default '0',
-  `item6` int(11) NOT NULL default '0',
-  `item7` int(11) NOT NULL default '0',
-  `item8` int(11) NOT NULL default '0',
-  `item9` int(11) NOT NULL default '0',
-  `itemCard` int(11) NOT NULL default '0',
-  `map` varchar(20) NOT NULL default 'prontera.gat',
-  PRIMARY KEY  (`drop_id`)
+  PRIMARY KEY  (`branch_id`),
+  INDEX (`account_id`),
+  INDEX (`char_id`)
 ) TYPE=MyISAM AUTO_INCREMENT=1 ;
 
 #Database: log
@@ -80,104 +63,6 @@ CREATE TABLE `mvplog` (
   PRIMARY KEY  (`mvp_id`)
 ) TYPE=MyISAM AUTO_INCREMENT=1 ;
 
-
-#Database: log
-#Table: presentlog
-CREATE TABLE `presentlog` (
-  `present_id` mediumint(9) unsigned NOT NULL auto_increment,
-  `present_date` datetime NOT NULL default '0000-00-00 00:00:00',
-  `src_id` tinyint(1) NOT NULL default '0',
-  `account_id` int(11) NOT NULL default '0',
-  `char_id` int(11) NOT NULL default '0',
-  `char_name` varchar(30) NOT NULL default '',
-  `nameid` int(11) NOT NULL default '0',
-  `map` varchar(20) NOT NULL default 'prontera.gat',
-  PRIMARY KEY  (`present_id`)
-) TYPE=MyISAM AUTO_INCREMENT=1 ;
-
-#Database: log
-#Table: producelog
-CREATE TABLE `producelog` (
-  `produce_id` mediumint(9) unsigned NOT NULL auto_increment,
-  `produce_date` datetime NOT NULL default '0000-00-00 00:00:00',
-  `account_id` int(11) NOT NULL default '0',
-  `char_id` int(11) NOT NULL default '0',
-  `char_name` varchar(30) NOT NULL default '',
-  `nameid` int(11) NOT NULL default '0',
-  `slot1` int(11) NOT NULL default '0',
-  `slot2` int(11) NOT NULL default '0',
-  `slot3` int(11) NOT NULL default '0',
-  `map` varchar(20) NOT NULL default 'prontera.gat',
-  `success` tinyint(1) NOT NULL default '0',
-  PRIMARY KEY  (`produce_id`)
-) TYPE=MyISAM AUTO_INCREMENT=1 ;
-
-#Database: log
-#Table: refinelog
-CREATE TABLE `refinelog` (
-  `refine_id` mediumint(9) unsigned NOT NULL auto_increment,
-  `refine_date` datetime NOT NULL default '0000-00-00 00:00:00',
-  `account_id` int(11) NOT NULL default '0',
-  `char_id` int(11) NOT NULL default '0',
-  `char_name` varchar(30) NOT NULL default '',
-  `nameid` int(11) NOT NULL default '0',
-  `refine` tinyint(2) NOT NULL default '0',
-  `card0` int(11) NOT NULL default '0',
-  `card1` int(11) NOT NULL default '0',
-  `card2` int(11) NOT NULL default '0',
-  `card3` int(11) NOT NULL default '0',
-  `map` varchar(20) NOT NULL default 'prontera.gat',
-  `success` tinyint(1) NOT NULL default '0',
-  `item_level` tinyint(2) NOT NULL default '0',
-  PRIMARY KEY  (`refine_id`)
-) TYPE=MyISAM AUTO_INCREMENT=1 ;
-
-#Database: log
-#Table: tradelog
-CREATE TABLE `tradelog` (
-  `trade_id` mediumint(9) unsigned NOT NULL auto_increment,
-  `trade_date` datetime NOT NULL default '0000-00-00 00:00:00',
-  `src_account_id` int(11) NOT NULL default '0',
-  `src_char_id` int(11) NOT NULL default '0',
-  `src_char_name` varchar(30) NOT NULL default '',
-  `des_account_id` int(11) NOT NULL default '0',
-  `des_char_id` int(11) NOT NULL default '0',
-  `des_char_name` varchar(30) NOT NULL default '',
-  `nameid` int(11) NOT NULL default '0',
-  `amount` int(11) NOT NULL default '1',
-  `refine` tinyint(4) NOT NULL default '0',
-  `card0` int(11) NOT NULL default '0',
-  `card1` int(11) NOT NULL default '0',
-  `card2` int(11) NOT NULL default '0',
-  `card3` int(11) NOT NULL default '0',
-  `map` varchar(20) NOT NULL default 'prontera.gat',
-  `zeny` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`trade_id`)
-) TYPE=MyISAM AUTO_INCREMENT=1 ;
-
-#Database: log
-#Table: vendlog
-CREATE TABLE `vendlog` (
-  `vend_id` mediumint(9) unsigned NOT NULL auto_increment,
-  `vend_date` datetime NOT NULL default '0000-00-00 00:00:00',
-  `vend_account_id` int(11) NOT NULL default '0',
-  `vend_char_id` int(11) NOT NULL default '0',
-  `vend_char_name` varchar(30) NOT NULL default '',
-  `buy_account_id` int(11) NOT NULL default '0',
-  `buy_char_id` int(11) NOT NULL default '0',
-  `buy_char_name` varchar(30) NOT NULL default '',
-  `nameid` int(11) NOT NULL default '0',
-  `amount` int(11) NOT NULL default '1',
-  `refine` tinyint(4) NOT NULL default '0',
-  `card0` int(11) NOT NULL default '0',
-  `card1` int(11) NOT NULL default '0',
-  `card2` int(11) NOT NULL default '0',
-  `card3` int(11) NOT NULL default '0',
-  `map` varchar(20) NOT NULL default 'prontera.gat',
-  `zeny` int(11) NOT NULL default '0',
-  KEY `vend_id` (`vend_id`)
-) TYPE=MyISAM AUTO_INCREMENT=1 ;
-
 #Database: log
 #Table: atcommandlog
 CREATE TABLE `atcommandlog` (
@@ -188,7 +73,9 @@ CREATE TABLE `atcommandlog` (
   `char_name` varchar(30) NOT NULL default '',
   `map` varchar(20) NOT NULL default 'prontera.gat',
   `command` varchar(50) NOT NULL default '',
-  PRIMARY KEY  (`atcommand_id`)
+  PRIMARY KEY  (`atcommand_id`),
+  INDEX (`account_id`),
+  INDEX (`char_id`)
 ) TYPE=MyISAM AUTO_INCREMENT=1 ;
 
 #Database: log
@@ -201,9 +88,10 @@ CREATE TABLE `npclog` (
   `char_name` varchar(30) NOT NULL default '',
   `map` varchar(20) NOT NULL default 'prontera.gat',
   `mes` varchar(255) NOT NULL default '',
-  PRIMARY KEY  (`npc_id`)
+  PRIMARY KEY  (`npc_id`),
+  INDEX (`account_id`),
+  INDEX (`char_id`)
 ) TYPE=MyISAM AUTO_INCREMENT=1 ;
-
 
 #ChatLOG
 CREATE TABLE `chatlog` (
@@ -218,5 +106,7 @@ CREATE TABLE `chatlog` (
   `src_map_y` smallint(4) NOT NULL default '0',
   `dst_charname` varchar(25) NOT NULL default '',
   `message` varchar(150) NOT NULL default '',
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY  (`id`),
+  INDEX (`src_accountid`),
+  INDEX (`src_charid`)
 ) TYPE=MyISAM AUTO_INCREMENT=1 ;
