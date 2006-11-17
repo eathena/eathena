ba3db0c13383c4ddad4e09fb5edd41521c24a91c
Skotlex <skotlex@mailforce.net>
2006-11-17 15:12:07 +0000

    - Fixed client not validating the chat-kick-request packet, which can cause crashes. - Merged some cleanups in chat.c
    git-svn-id: http://eathena.googlecode.com/svn/branches/stable@9244 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- src/map/chat.c
+++ src/map/chat.c
@@ -120,8 +120,10 @@ int chat_leavechat(struct map_session_data *sd)
 	nullpo_retr(1, sd);
 
 	cd=(struct chat_data*)map_id2bl(sd->chatID);
-	if(cd==NULL)
+	if(cd==NULL) {
+		sd->chatID = 0;
 		return 1;
+	}
 
 	for(i = 0,leavechar=-1;i < cd->users;i++){
 		if(cd->usersd[i] == sd){
@@ -129,8 +131,11 @@ int chat_leavechat(struct map_session_data *sd)
 			break;
 		}
 	}
-	if(leavechar<0)	// そのchatに所属していないらしい (バグ時のみ)
+	if(leavechar<0)
+  	{	//Not found in the chatroom?
+		sd->chatID = 0;
 		return -1;
+	}
 
 	if(leavechar==0 && cd->users>1 && (*cd->owner)->type==BL_PC){
 		// 所有者だった&他に人が居る&PCのチャット
@@ -148,18 +153,19 @@ int chat_leavechat(struct map_session_data *sd)
 		//Delete empty chatroom
 		clif_clearchat(cd,0);
 		map_delobject(cd->bl.id);
-	} else {
-		for(i=leavechar;i < cd->users;i++)
-			cd->usersd[i] = cd->usersd[i+1];
-		if(leavechar==0 && (*cd->owner)->type==BL_PC){
-			//Adjust Chat location after owner has been changed.
-			map_delblock( &cd->bl );
-			cd->bl.x=cd->usersd[0]->bl.x;
-			cd->bl.y=cd->usersd[0]->bl.y;
-			map_addblock( &cd->bl );
-		}
-		clif_dispchat(cd,0);
+		return 1;
 	}
+	for(i=leavechar;i < cd->users;i++)
+		cd->usersd[i] = cd->usersd[i+1];
+
+	if(leavechar==0 && (*cd->owner)->type==BL_PC){
+		//Adjust Chat location after owner has been changed.
+		map_delblock( &cd->bl );
+		cd->bl.x=cd->usersd[0]->bl.x;
+		cd->bl.y=cd->usersd[0]->bl.y;
+		map_addblock( &cd->bl );
+	}
+	clif_dispchat(cd,0);
 
 	return 0;
 }
@@ -251,6 +257,8 @@ int chat_kickchat(struct map_session_data *sd,char *kickusername)
 
 	cd = (struct chat_data *)map_id2bl(sd->chatID);
 	
+	if (!cd) return -1;
+
 	for(i = 0; i < cd->users; i++) {
 		if (strcmp(cd->usersd[i]->status.name, kickusername) == 0) {
 			if (battle_config.gm_kick_chat && pc_isGM(cd->usersd[i]) >= battle_config.gm_kick_chat)
