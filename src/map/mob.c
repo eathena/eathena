a145ce0b8e017e18f1acc660cb902eda4d69670a
Skotlex <skotlex@mailforce.net>
2007-04-14 06:26:21 +0000

    - Fixed a null pointer when a mob needs to unlock a target while it's idle.
    git-svn-id: http://eathena.googlecode.com/svn/branches/stable@10254 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- src/map/mob.c
+++ src/map/mob.c
@@ -996,10 +996,12 @@ int mob_unlocktarget(struct mob_data *md,int tick)
 		if (battle_config.mob_ai&0x8)
 			mob_stop_walking(md,1); //Inmediately stop chasing.
 		md->state.skillstate = MSS_IDLE;
-		md->target_id=0;
-		md->ud.target = 0;
 		md->next_walktime=tick+rand()%3000+3000;
 		break;
+	}
+	if (md->target_id) {
+		md->target_id=0;
+		md->ud.target = 0;
 	}
 	return 0;
 }
@@ -1110,8 +1112,6 @@ static int mob_ai_sub_hard(struct block_list *bl,va_list ap)
 				((((TBL_PC*)tbl)->state.gangsterparadise && !(mode&MD_BOSS)) ||
 				((TBL_PC*)tbl)->invincible_timer != INVALID_TIMER)
 		)) {	//Unlock current target.
-			if (battle_config.mob_ai&0x8) //Inmediately stop chasing.
-				mob_stop_walking(md,1);
 			mob_unlocktarget(md, tick-(battle_config.mob_ai&0x8?3000:0)); //Imediately do random walk.
 			tbl = NULL;
 		}
