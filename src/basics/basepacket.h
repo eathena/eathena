a19d852f68f4d79d8b6dc99911e57159fe680a0a
Flávio J. Saraiva <flaviojs2005@gmail.com>
2007-05-18 02:00:53 +0000

    * Concept of how a packet works internally.
    git-svn-id: http://eathena.googlecode.com/svn/branches/eapp@10560 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- src/basics/basepacket.h
+++ src/basics/basepacket.h
@@ -8,20 +8,85 @@
 NAMESPACE_BEGIN(basics)
 ///////////////////////////////////////////////////////////////////////////////
 
+/*
+
+	<< Packets >>
+
+Concept:
+	A packet has an internal buffer with data and a set of fields.
+	Every field of the packet is a public object in the packet.
+	These objects point to their corresponding offsets in the packet buffer.
+
+	The number of fields is fixed by design.
+	When there is a dynamic number of fields in a packet, they are grouped 
+	together as a dynamic field.
+	ex: an array of inventory items
+
+	The packet contains an array with pointers to the field objects in-order.
+	Each field knows it's size, so to get the offsets we just iterate 
+	the array and add-up the sizes.
+
+	Resizing a dynamic field:
+	- the data of the field is being changed so the field packs it's data if necessary
+	  ex: removing an entry in the middle of an array
+	- the field invokes packetbase::on_resize(packetfield& field, size_t new_len)
+	  with itself and the new length.
+	- the packet reallocs, moves data around and updates the buffers of the 
+	  fields as necessary.
+	- the field updates it's length and does final changes to the data
+
+*/
+
 
 ///////////////////////////////////////////////////////////////////////////////
-/// base packet.
+/// base packet
 class packetbase
 {
+
+	friend class packetfield;
+
 protected:
+	// internal buffer with packet data
 	uint8* _buf;
 	size_t _len;
+	size_t _max;
+
+	// array of fields in-order
+	packetfield* _fields;
+	size_t _fieldcount;
+
+	///////////////////////////////////////////////////////////////////////////
+	/// One of the fields was resized.
+	/// Move data around or realloc the buffer if necessary.
+	void on_resize(packetfield& field, size_t new_len);
+
 public:
-	/// Length of the packet
+	///////////////////////////////////////////////////////////////////////////
+	/// Length of the packet data
 	size_t length() const;
 	/// Data of the packet
 	const uint8* buffer() const;
 };
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Field of a packet
+class packetfield
+{
+
+	friend class packetbase;
+
+protected:
+	// pointer to the data
+	uint8* _buf;
+
+public:
+	///////////////////////////////////////////////////////////////////////////
+	/// Length of data
+	virtual size_t length() const=0;
+};
+
 
 
 ///////////////////////////////////////////////////////////////////////////////
