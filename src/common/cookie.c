a29a0436ced51b1fbbab46bf42f5bdfbd8455fe2
Flávio J. Saraiva <flaviojs2005@gmail.com>
2009-11-07 23:59:46 +0000

    * Added cookie handling to common. (will be used by the reconnect code)
    git-svn-id: http://eathena.googlecode.com/svn/branches/charmerge@14135 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

---
+++ src/common/cookie.c
@@ -0,0 +1,98 @@
+// Copyright (c) Athena Dev Teams - Licensed under GNU GPL
+// For more information, see LICENCE in the main folder
+
+#include "../common/cbasetypes.h"
+#include "../common/md5calc.h"
+#include "../common/timer.h"
+#include "cookie.h"
+
+#include <stdlib.h> // rand
+#include <string.h> // memset,memcpy
+
+
+/// Timeout timer.
+/// Triggers when the cookie expires.
+/// Either calls the callback or resets the cookie data.
+static int cookie_timeout_timer(int tid, unsigned int tick, int id, intptr data)
+{
+	struct s_cookie* cookie = (struct s_cookie*)data;
+	if( cookie == NULL || cookie->timeout_timer != tid )
+		return 0;// invalid
+
+	cookie->timeout_timer = INVALID_TIMER;
+	if( cookie->on_timeout != NULL )
+		cookie->on_timeout(cookie->callback_data);
+	else
+	{
+		cookie->len = 0;
+		memset(cookie->data, 0, MAX_COOKIE_LEN);
+	}
+	return 0;
+}
+
+
+/// Initializes the cookie.
+void cookie_init(struct s_cookie* cookie)
+{
+	cookie->len = 0;
+	memset(cookie->data, 0, MAX_COOKIE_LEN);
+	cookie->timeout = 0;
+	cookie->timeout_timer = INVALID_TIMER;
+	cookie->callback_data = 0;
+	cookie->on_timeout = NULL;
+}
+
+
+/// Destroys the cookie.
+void cookie_destroy(struct s_cookie* cookie)
+{
+	cookie_timeout_stop(cookie);
+	cookie_init(cookie);
+}
+
+
+/// Generates random cookie data.
+void cookie_generate(struct s_cookie* cookie)
+{
+	cookie->len = (uint16)(MAX_COOKIE_LEN/2 + rand() % (MAX_COOKIE_LEN/2));
+	memset(cookie->data, 0, MAX_COOKIE_LEN);
+	MD5_Salt(cookie->len, cookie->data);
+}
+
+
+/// Sets the cookie data.
+/// Truncates the data if too big.
+void cookie_set(struct s_cookie* cookie, uint16 len, char* data)
+{
+	if( len > MAX_COOKIE_LEN )
+		len = MAX_COOKIE_LEN;
+	cookie->len = len;
+	memset(cookie->data, 0, MAX_COOKIE_LEN);
+	memcpy(cookie->data, data, len);
+}
+
+
+/// Starts the timeout timer.
+void cookie_timeout_start(struct s_cookie* cookie)
+{
+	static bool is_registered = false;
+	if( !is_registered )
+	{
+		add_timer_func_list(cookie_timeout_timer, "cookie_timeout_timer");
+		is_registered = true;
+	}
+
+	if( cookie->timeout_timer != INVALID_TIMER )
+		return;// already running
+	cookie->timeout_timer = add_timer(gettick() + cookie->timeout, cookie_timeout_timer, 0, (intptr)cookie);
+}
+
+
+/// Stops the timeout timer.
+void cookie_timeout_stop(struct s_cookie* cookie)
+{
+	if( cookie->timeout_timer == INVALID_TIMER )
+		return;// not running
+	delete_timer(cookie->timeout_timer, cookie_timeout_timer);
+	cookie->timeout = INVALID_TIMER;
+}
---
+++ src/common/cookie.h
@@ -0,0 +1,34 @@
+// Copyright (c) Athena Dev Teams - Licensed under GNU GPL
+// For more information, see LICENCE in the main folder
+
+#ifndef _COMMON_COOKIE_H_
+#define _COMMON_COOKIE_H_
+
+#ifndef _CBASETYPES_H_
+#include "../common/cbasetypes.h"
+#endif
+
+typedef void (*f_cookie_callback)(intptr data);
+
+#define MAX_COOKIE_LEN 256
+struct s_cookie
+{
+	uint16 len;
+	char data[MAX_COOKIE_LEN];
+
+	uint32 timeout; //< How long the cookie lasts for before expiring
+	int timeout_timer;
+
+	intptr callback_data; //< user-defined data
+	f_cookie_callback on_timeout; //< Called when the cookie expires. If NULL, the cookie data is reset instead.
+};
+
+void cookie_init(struct s_cookie* cookie);
+void cookie_destroy(struct s_cookie* cookie);
+
+void cookie_generate(struct s_cookie* cookie);
+void cookie_set(struct s_cookie* cookie, uint16 len, char* data);
+void cookie_timeout_start(struct s_cookie* cookie);
+void cookie_timeout_stop(struct s_cookie* cookie);
+
+#endif /* _COMMON_COOKIE_H_ */
