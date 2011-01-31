eathena/eathena

commit e9b5a4c0920219dfac1011520545a966169ff0c6

* Fixed a map-server crash when invalid items are deleted (option 'item_check') from a logging-in character (bugreport:2604). - This removes the pc_checkitem call from pc_authok, as the check is done in clif_parse_LoadEndAck as well, after first status calc has taken place. - Inventory list is now sent before pc_checkitem in clif_parse_LoadEndAck, so that deleted items do not show up as 'unknown item'.
git-svn-id: http://eathena.googlecode.com/svn/trunk@14685 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

src/map/pc.c
@@ -892,9 +892,7 @@ bool pc_authok(struct map_session_data *sd, int login_id2, time_t expiration_tim
 	if (!(battle_config.display_skill_fail&2))
 		sd->state.showdelay = 1;
 		
-	// アイテムチェック
 	pc_setinventorydata(sd);
-	pc_checkitem(sd);
 
 	status_change_init(&sd->bl);
 	if ((battle_config.atc_gmonly == 0 || pc_isGM(sd)) && (pc_isGM(sd) >= get_atcommand_level(atcommand_hide)))

src/map/clif.c
@@ -8400,8 +8400,8 @@ void clif_parse_LoadEndAck(int fd,struct map_session_data *sd)
 		clif_refreshlook(&sd->bl,sd->bl.id,LOOK_CLOTHES_COLOR,sd->vd.cloth_color,SELF);
 
 	// item
+	clif_inventorylist(sd);  // inventory list first, otherwise deleted items in pc_checkitem show up as 'unknown item'
 	pc_checkitem(sd);
-	clif_inventorylist(sd);
 	
 	// cart
 	if(pc_iscarton(sd)) {

commit 029e29081b868a6ecd41203cc3ab1be6f6b3aadb

* Inventory and Cart item arrays are no longer getting compacted on each log-in/teleport/warp. - This also resolves yet another issue caused by r14685 (bugreport:2604).
git-svn-id: http://eathena.googlecode.com/svn/trunk@14691 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

src/map/pc.c
@@ -7308,55 +7308,38 @@ int pc_unequipitem(struct map_session_data *sd,int n,int flag)
  *------------------------------------------*/
 int pc_checkitem(struct map_session_data *sd)
 {
-	int i,j,k,id,calc_flag = 0;
+	int i,id,calc_flag = 0;
 	struct item_data *it=NULL;
 
 	nullpo_ret(sd);
 
 	if( sd->vender_id ) //Avoid reorganizing items when we are vending, as that leads to exploits (pointed out by End of Exam)
 		return 0;
-	
-	for( i = j = 0; i < MAX_INVENTORY; i++ )
-	{
-		if( (id = sd->status.inventory[i].nameid) == 0 )
-			continue;
 
-		if( battle_config.item_check && !itemdb_available(id) )
+	if( battle_config.item_check )
+	{// check for invalid(ated) items
+		for( i = 0; i < MAX_INVENTORY; i++ )
 		{
-			ShowWarning("illegal item id %d in %d[%s] inventory.\n",id,sd->bl.id,sd->status.name);
-			pc_delitem(sd,i,sd->status.inventory[i].amount,3,0);
-			continue;
+			id = sd->status.inventory[i].nameid;
+
+			if( id && !itemdb_available(id) )
+			{
+				ShowWarning("Removed invalid/disabled item id %d from inventory (amount=%d, char_id=%d).\n", id, sd->status.inventory[i].amount, sd->status.char_id);
+				pc_delitem(sd, i, sd->status.inventory[i].amount, 0, 0);
+			}
 		}
-		if( i > j )
+
+		for( i = 0; i < MAX_CART; i++ )
 		{
-			memcpy(&sd->status.inventory[j], &sd->status.inventory[i], sizeof(struct item));
-			sd->inventory_data[j] = sd->inventory_data[i];
+			id = sd->status.cart[i].nameid;
+
+			if( id && !itemdb_available(id) )
+			{
+				ShowWarning("Removed invalid/disabled item id %d from cart (amount=%d, char_id=%d).\n", id, sd->status.cart[i].amount, sd->status.char_id);
+				pc_cart_delitem(sd, i, sd->status.cart[i].amount, 0);
+			}
 		}
-		j++;
 	}
-
-	if( j < MAX_INVENTORY )
-		memset(&sd->status.inventory[j], 0, sizeof(struct item)*(MAX_INVENTORY-j));
-	for( k = j ; k < MAX_INVENTORY; k++ )
-		sd->inventory_data[k] = NULL;
-
-	for( i = j = 0; i < MAX_CART; i++ )
-	{
-		if( (id=sd->status.cart[i].nameid) == 0 )
-			continue;
-		if( battle_config.item_check &&  !itemdb_available(id) ){
-			ShowWarning("illegal item id %d in %d[%s] cart.\n",id,sd->bl.id,sd->status.name);
-			pc_cart_delitem(sd,i,sd->status.cart[i].amount,1);
-			continue;
-		}
-		if( i > j )
-		{
-			memcpy(&sd->status.cart[j],&sd->status.cart[i],sizeof(struct item));
-		}
-		j++;
-	}
-	if( j < MAX_CART )
-		memset(&sd->status.cart[j],0,sizeof(struct item)*(MAX_CART-j));
 
 	for( i = 0; i < MAX_INVENTORY; i++)
 	{
@@ -7389,7 +7372,6 @@ int pc_checkitem(struct map_session_data *sd)
 		}
 	}
 
-	pc_setequipindex(sd);
 	if( calc_flag && sd->state.active )
 	{
 		pc_checkallowskill(sd);