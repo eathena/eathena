2a8b014a4721e9a59fc8b40f38d695ae19287328
ultramage <ultramage@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2011-12-04 13:14:54 +0000

    Moved DES code from grfio to separate file.
    git-svn-id: http://eathena-project.googlecode.com/svn/trunk@15007 54d463be-8e91-2dee-dedb-b68131a5f0ec

---
+++ src/common/des.h
@@ -0,0 +1,10 @@
+// Copyright (c) Athena Dev Teams - Licensed under GNU GPL
+// For more information, see LICENCE in the main folder
+#ifndef	_DES_H_
+#define	_DES_H_
+
+
+void des_decode(unsigned char* buf);
+
+
+#endif // _DES_H_
