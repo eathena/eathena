2eb3d11ba5c931bad625f2c40906bfccc30a97e3
flaviojs <flaviojs@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2012-06-13 04:30:40 +0000

    CMake: fix NULL not available with only these headers
    git-svn-id: http://eathena-project.googlecode.com/svn/trunk@15134 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- 3rdparty/cmake/tests/MYSQL_TRY_RUN.c
+++ 3rdparty/cmake/tests/MYSQL_TRY_RUN.c
@@ -4,8 +4,8 @@
 #include "mysql.h"
 int main(int argc, char** argv)
 {
-	MYSQL* h = mysql_init(NULL);
-	if(h == NULL) return 1;//error
+	MYSQL* h = mysql_init(0);
+	if(h == 0) return 1;//error
 	mysql_close(h);
 	return 0;
 }
