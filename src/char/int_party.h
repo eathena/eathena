56c16b245d6869a2e31509d76c41b126300da7ce
Flávio J. Saraiva <flaviojs2005@gmail.com>
2012-06-27 15:24:29 +0000

    Send party to map-server before the player.
    git-svn-id: http://eathena.googlecode.com/svn/trunk@15139 9c4de8d1-4af9-ca9b-867f-b47ac08f6c80

--- src/char/int_party.h
+++ src/char/int_party.h
@@ -5,12 +5,14 @@
 #define _INT_PARTY_H_
 
 struct party;
+struct mmo_charstatus;
 
 int inter_party_init(void);
 void inter_party_final(void);
 int inter_party_save(void);
 int inter_party_parse_frommap(int fd);
 int inter_party_leave(int party_id,int account_id, int char_id);
+bool inter_party_update(struct mmo_charstatus* cd);
 
 extern char party_txt[1024];
 
--- src/char_sql/int_party.h
+++ src/char_sql/int_party.h
@@ -19,6 +19,7 @@
 #define PS_BREAK 0x20
 
 struct party;
+struct mmo_charstatus;
 
 int inter_party_parse_frommap(int fd);
 int inter_party_sql_init(void);
@@ -26,6 +27,8 @@ void inter_party_sql_final(void);
 int inter_party_leave(int party_id,int account_id, int char_id);
 int inter_party_CharOnline(int char_id, int party_id);
 int inter_party_CharOffline(int char_id, int party_id);
+bool inter_party_update(struct mmo_charstatus* cd);
+
 //Required for the TXT->SQL converter
 int inter_party_tosql(struct party *p, int flag, int index);
 
